499fee6d1ddd035196693d78980bdbf0657c4ba9
Andrew Sack <andrew.sack@tufts.edu>
2019-09-13 13:43:02 -0400

    added stuff

--- HW2/hw2.py
+++ HW2/hw2.py
@@ -23,11 +23,16 @@
 #  
 
 import time
+from adafruit_servokit import ServoKit
+kit = ServoKit(channels=16)
 
 def main(args):
     while True:
         print(time.localtime())
-        time.sleep(5)
+        kit.servo[0].angle = 180
+        time.sleep(2)
+        kit.servo[0].angle = 0
+        time.sleep(2)
     return 0
 
 if __name__ == '__main__':
