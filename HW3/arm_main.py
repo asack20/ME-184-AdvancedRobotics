3c47796afcc443575c854ed1f621346ca7da7e63
Andrew Sack <andrew.sack@tufts.edu>
2019-10-01 19:44:06 -0400

    Update arm_main.py

--- HW3/arm_main.py
+++ HW3/arm_main.py
@@ -6,9 +6,9 @@ from time import sleep
 import numpy as np
 import math as m
 import sys #for command line arguments
-
 from adafruit_servokit import ServoKit  #uses servo shield api to simplify PWM controll of servos
 kit = ServoKit(channels=16) #specify model being used (16 port version)
+
 arm1 = kit.servo[0];
 arm2 = kit.servo[1];
 pen = kit.servo[2];
@@ -17,13 +17,13 @@ pen = kit.servo[2];
 l1 = 6.48; #inches
 l2 = 6.48; #inches
 
-
-
-
+# Verify command line input
 filename = sys.argv[1];
 if len(sys.argv) != 2:
     sys.exit("Incorrect number of arguments")
 
+###########################################
+# File Input
 
 # initalize lists as empty
 x_pos_temp = []
@@ -46,21 +46,17 @@ for i in range(0, size):
     isWrite_temp.append(bool(int(line[2])))
 # end for
 
-
 x_in = np.array(x_pos_temp)
 y_in = np.array(y_pos_temp)
 write_In = np.array(isWrite_temp)
 
 print('done')
 
-
+################################################
+# Interpolate betwwen points
 print('Interpolating more coordinates...', end='')
 n_steps = 20
 
-print(len(x_in))
-
-#x_pos = np.array(x_in[0])
-#y_pos = np.array(y_in[0])
 for i in range(0, len(x_in)-1):
     x_interp = np.linspace(x_in[i],x_in[i+1], n_steps)
     y_interp = np.linspace(y_in[i],y_in[i+1], n_steps)
@@ -75,39 +71,11 @@ for i in range(0, len(x_in)-1):
         isWrite = np.concatenate((isWrite, write_interp), axis=None)
 
 print('done')
-#isWrite = np.full(np.size(x_pos), True)
 
+##############################################
+# Motion
 
-print(x_pos.size)
-print('Calculating servo target angles...', end='')
-
-# cos_2 = (np.square(x_pos) + np.square(y_pos) - np.square(l1) - np.square(l2))/(2*l1*l2)
-# sin_2 = -np.sqrt(1-np.square(cos_2))
-# theta_2 = np.degrees(np.arctan2(sin_2,cos_2))
-
-# sin_1 = ((l1 + l2*cos_2)*y_pos - (l2*sin_2*x_pos)) / (np.square(x_pos) + np.square(y_pos))
-# cos_1 = ((l1 + l2*cos_2)*x_pos - (l2*sin_2*y_pos)) / (np.square(x_pos) + np.square(y_pos))
-# theta_1 = np.degrees(np.arctan2(sin_1,cos_1))
-
-# ###
-# # calculate motor angles from position all in radians
-# cos_alpha = (np.square(l2) - (np.square(l1) + (np.square(x_pos) + np.square(y_pos))))/(-2*l1*np.sqrt(np.square(x_pos) + np.square(y_pos)))
-# #print(cos_alpha) 
-# alpha_calc = np.arctan2(np.sqrt(1 - np.square(cos_alpha)), cos_alpha)
-# beta_calc = np.arctan2(y_pos, x_pos)
-
-# theta_1_r = alpha_calc + beta_calc
-
-# cos_theta = ((np.square(x_pos) + np.square(y_pos)) - (np.square(l1) + np.square(l2)))/(-2*l1*l2)
-# theta_2_r = -np.arctan2(np.sqrt(1 - np.square(cos_theta)),cos_theta)
-
-# # target motor position in degrees
-# theta_1 = np.degrees(theta_1_r)
-# theta_2 = np.degrees(theta_2_r) 
-theta_3 = 90*isWrite
-
-print('done')
-
+# Move to zero position
 print('Initializing arm postion...', end='')
 prev_1 = 90
 prev_2 = 90
@@ -119,17 +87,18 @@ sleep(3)
 print('done')
 
 
+# Run through points
 print('Running motion...')
 for i in range(0, len(x_pos)):
+    # Inverse Kinemetics Calculations
     cos_2 = (x_pos[i]**2 + y_pos[i]**2 - l1**2 - l2**2)/(2*l1*l2)
     sin_2 = m.sqrt(1 - cos_2**2)
     theta_2 = m.degrees(m.atan2(sin_2,cos_2))
-
     theta_1 = m.degrees(m.atan2(y_pos[i],x_pos[i]) - m.atan2(l2*sin_2,l1+l2*cos_2))
         
-    
     theta_3 = 90*isWrite[i]
     
+    # Print to Command Line
     if True:    
         print('Step: ' + str(i))
         print('\t X Pos: '+ str(x_pos[i]))
@@ -138,10 +107,12 @@ for i in range(0, len(x_pos)):
         print('\t Arm 2: '+ str(theta_2) + '\t' +str(theta_2%180))
         print('\t Pen: '+ str(isWrite[i]))
 
+    # Actuate Pen
     if (theta_3 != prev_3):
         pen.angle = theta_3
         sleep(1)  
 
+    # Command servos to move
     arm1.angle = theta_1 % 180
     arm2.angle = (theta_2)  % 180
 
@@ -149,6 +120,6 @@ for i in range(0, len(x_pos)):
     prev_2 = theta_2
     prev_3 = theta_3
     
-    sleep(0.1)
+    sleep(0.1) # sleep 0.1 sec between moves
 
 print('done')
