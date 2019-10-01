99d983b615319c8c4c6c68a0e5b0bff9eb583e6f
Andrew Sack <andrew.sack@tufts.edu>
2019-10-01 13:14:20 -0400

    Update arm_main.py

--- HW3/arm_main.py
+++ HW3/arm_main.py
@@ -55,7 +55,7 @@ print('done')
 
 
 print('Interpolating more coordinates...', end='')
-n_steps = 10
+n_steps = 20
 
 print(len(x_in))
 
@@ -143,7 +143,7 @@ for i in range(0, len(x_pos)):
         sleep(1)  
 
     arm1.angle = theta_1 % 180
-    arm2.angle = theta_2 % 180
+    arm2.angle = (theta_2 * -1)  % 180
 
     prev_1 = theta_1
     prev_2 = theta_2
