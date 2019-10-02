0204cca47ebe6042845822ceb651e9f40f3bd0ec
Andrew Sack <andrew.sack@tufts.edu>
2019-10-02 17:44:27 -0400

    Update imu_test.py

--- HW4/imu_test.py
+++ HW4/imu_test.py
@@ -1,40 +1,15 @@
-# coding: utf-8
-## @package faboMPU9250
-#  This is a library for the FaBo 9AXIS I2C Brick.
-#
-#  http://fabo.io/202.html
-#
-#  Released under APACHE LICENSE, VERSION 2.0
-#
-#  http://www.apache.org/licenses/
-#
-#  FaBo <info@fabo.io>
+import utime
+from machine import I2C, Pin
+from mpu9250 import MPU9250
 
-import FaBo9Axis_MPU9250
-import time
-import sys
+i2c = I2C(scl=Pin(22), sda=Pin(21))
+sensor = MPU9250(i2c)
 
-mpu9250 = FaBo9Axis_MPU9250.MPU9250()
+print("MPU9250 id: " + hex(sensor.whoami))
 
-try:
-    while True:
-        accel = mpu9250.readAccel()
-        print(" ax = " , ( accel['x'] ))
-        print(" ay = " , ( accel['y'] ))
-        print(" az = " , ( accel['z'] ))
+while True:
+    print(sensor.acceleration)
+    print(sensor.gyro)
+    print(sensor.magnetic)
 
-        gyro = mpu9250.readGyro()
-        print(" gx = " , ( gyro['x'] ))
-        print(" gy = " , ( gyro['y'] ))
-        print(" gz = " , ( gyro['z'] ))
-
-        mag = mpu9250.readMagnet()
-        print(" mx = " , ( mag['x'] ))
-        print(" my = " , ( mag['y'] ))
-        print(" mz = " , ( mag['z'] ))
-        print()
-
-        time.sleep(0.5)
-
-except KeyboardInterrupt:
-    sys.exit()
+    utime.sleep_ms(1000)
