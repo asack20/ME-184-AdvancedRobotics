274a143730741c4421335760694176f8f9568dab
Andrew Sack <andrew.sack@tufts.edu>
2019-10-09 17:35:22 -0400

    Update bluetooth_send.py

--- HW4/bluetooth_send.py
+++ HW4/bluetooth_send.py
@@ -1,25 +1,43 @@
 import bluetooth
 import time
 
-def sendMessageTo(targetBluetoothMacAddress, message):
+def connectTo(targetBluetoothMacAddress):
   port = 1
   sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
   sock.connect((targetBluetoothMacAddress, port))
-  sock.send(message)
-  sock.close()
+  sock.send("Connected from: " + targetBluetoothMacAddress)
+  
+  return sock
+
+
+
+def sendMessageTo(sock, message):
+  sock.send("Connected from: " + mac)
+
+
 
 
 def main(args):
 	mac = "B8:27:EB:64:6F:44"
 	message = "testing testing"
+	isConnected = False
 
-	while True:
-	    print("Test")
+	while !isConnected:
+	    print("Attempting connection to: " + str(mac))
 	    try:
-	    	sendMessageTo(mac, message)
+	    	sock = sendMessageTo(mac, message)
+	    	isConnected = True
 	    except:
-	    	print("No Send")
-	    time.sleep(2)
+	    	print("Could not connect. Will try again in 2 seconds")
+	    	isConnected = False
+	    	time.sleep(2)
+	print("Connected to: " + str(mac) + "\n")
+
+	for i in range(0, 10):
+		sendMessageTo(sock, str(i))
+
+	sock.close()
+
 	return 0
 
 if __name__ == '__main__':
