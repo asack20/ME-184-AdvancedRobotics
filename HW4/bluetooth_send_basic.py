acb84e979f583c63205d5e1e9a96d4913f2166da
Andrew Sack <andrew.sack@tufts.edu>
2019-10-09 17:46:48 -0400

    Create bluetooth_send_basic.py

---
+++ HW4/bluetooth_send_basic.py
@@ -0,0 +1,27 @@
+import bluetooth
+import time
+
+def sendMessageTo(targetBluetoothMacAddress, message):
+  port = 1
+  sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+  sock.connect((targetBluetoothMacAddress, port))
+  sock.send(message)
+  sock.close()
+
+
+def main(args):
+	mac = "B8:27:EB:64:6F:44"
+	message = "testing testing"
+
+	while True:
+	    print("Test")
+	    try:
+	    	sendMessageTo(mac, message)
+	    except:
+	    	print("No Send")
+	    time.sleep(2)
+	return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
