da00ffc411f350d80c348bf7a2c1b76ee2ef2235
Andrew Sack <andrew.sack@tufts.edu>
2019-11-05 18:55:26 -0500

    hw5

--- HW5/close_gripper.py
+++ HW5/close_gripper.py
@@ -3,12 +3,7 @@ import board, digitalio, busio
 from adafruit_servokit import ServoKit
 kit = ServoKit(channels=16)
 
-def main(args):
-    print("Closing Gripper")
-    kit.servo[0].angle = 180
-    time.sleep(2000)
-    return 0
 
-if __name__ == '__main__':
-    import sys
-    sys.exit(main(sys.argv))
+print("Closing Gripper")
+kit.servo[0].angle = 180
+time.sleep(2000)
--- HW5/open_gripper.py
+++ HW5/open_gripper.py
@@ -3,12 +3,8 @@ import board, digitalio, busio
 from adafruit_servokit import ServoKit
 kit = ServoKit(channels=16)
 
-def main(args):
-    print("Opening Gripper")
-    kit.servo[0].angle = 0
-    time.sleep(2000)
-    return 0
 
-if __name__ == '__main__':
-    import sys
-    sys.exit(main(sys.argv))
+print("Opening Gripper")
+kit.servo[0].angle = 0
+time.sleep(2000)
+
