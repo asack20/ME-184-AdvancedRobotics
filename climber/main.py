60b1c539fc8dab3db5f2a27e53d6f8e7f71e581f
Andrew Sack <andrew.sack@tufts.edu>
2019-12-08 13:13:40 -0500

    Added basic motion code

--- climber/main.py
+++ climber/main.py
@@ -9,35 +9,124 @@ from edge_detection import detectEdge
 import numpy as np
 from adafruit_servokit import ServoKit
 import time
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)#Button to GPIO23
+open_button = 23
+closed_button = 24 
 
 kit = ServoKit(channels=16) #specify model being used (16 port version)
 
-left = kit.servo[0];
-right = kit.servo[1];
+TL = kit.servo[0];
+TR = kit.servo[1];
 
-def open_gripper():
-    left.angle = 150
-    right.angle = 0
+BL = kit.servo[2];
+BR = kit.servo[3];
+
+CL = kit.continuous_servo[4];
+CR = kit.continuous_servo[5];
+
+def open_top_gripper():
+    TL.angle = 150
+    TR.angle = 0
     
     return 0
 
-def close_gripper():
+def close_top_gripper():
     dist = detectEdge();
-    left.angle = 0
-    right.angle = 180
+    TL.angle = 0
+    TR.angle = 180
     
     return 0
 
-while True:
+def open_bottom_gripper():
+    BL.angle = 150
+    BR.angle = 0
+    
+    return 0
+
+def close_bottom_gripper():
+    dist = detectEdge();
+    BL.angle = 0
+    BR.angle = 180
+    
+    return 0
+
+def expand():
+    button_pressed = not GPIO.input(open_button)
+    while not button_pressed:
+        CR.throttle = 0.5
+        CL.throttle = 0.5
+        button_pressed = not GPIO.input(open_button)
+    
+    CR.throttle = 0
+    CL.throttle = 0
+        
+    return 0
+
+def contract():
+    button_pressed = not GPIO.input(closed_button)
+    while not button_pressed:
+        CR.throttle = -0.5
+        CL.throttle = -0.5
+        button_pressed = not GPIO.input(closed_button)
+    
+    CR.throttle = 0
+    CL.throttle = 0
+        
+    return 0
+
+def zero_motion():
+    CR.throttle = 0
+    CL.throttle = 0
+
+    open_bottom_gripper()
+    open_top_gripper()
+
+    return 0
+
+def climb_move():
+    close_top_gripper()
+    time.sleep(2)
+    open_bottom_gripper()
+    time.sleep(2)
+    contract()
+    time.sleep(2)
+    close_bottom_gripper()
+    time.sleep(2)
+    open_top_gripper()
+    time.sleep(2)
+    expand()
+    time.sleep(2)
+    close_top_gripper()
+    time.sleep(2)
+
+    return 0
+
+##########################################
+# Script #################################
+##########################################
+
+print("Program Started")
+
+keep_going = True
+
+print("Zeroing Motion")
+zero_motion()
+time.sleep(2)
+
+while keep_going:
     command = ''
-    command = input('"c" to close, "o" to open\n')
-    if command == 'c':
-        print("Closing Gripper")
-        close_gripper()
-        time.sleep(2)
-    elif command == 'o':
-        print("Opening Gripper")
-        open_gripper()
+    command = input('"g" to go, "s" to stop\n')
+    if command == 'g':
+        print("Climbing Up")
+        climb_move()
+
+    elif command == 's':
+        print("Stopping")
+        keep_going = false
+        zero_motion()
         time.sleep(2)
     else:
         print("Invalid Command")
