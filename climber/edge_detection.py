90e525ac12a604dc7650a411c62808dacc96a7d7
Andrew Sack <andrew.sack@tufts.edu>
2019-12-11 17:40:55 -0500

    Update edge_detection.py

--- climber/edge_detection.py
+++ climber/edge_detection.py
@@ -13,14 +13,17 @@ import picamera
 
 # Perform material detection
 def closestMaterial(im):
-    im_color = np.mean(im, axis = (0,1))
+    im_color = np.mean(im, axis = (0,1)) # calc mean color of image
     cName = ['wood','carpet','brick','metal','tree']
+    # Mean color of each material
     rgb_ref = np.array([[137, 131, 114],[104,90,74],[106,122,125],[117, 141, 153],[123,123,117]])
     
     cDist = np.zeros((5))
-    for i in range(0,5):
+    for i in range(0,5): # Calculate distance to each material
         cDist[i] = np.sqrt(np.square(im_color[0] - rgb_ref[i,0]) +  np.square(im_color[1] - rgb_ref[i,1]) + np.square(im_color[2] - rgb_ref[i,2])   )
     
+
+    # Find closest material
     minDist = np.min(cDist)
     minInd = np.argmin(cDist)
     mat = cName[minInd]
@@ -30,6 +33,8 @@ def closestMaterial(im):
 # Perform edge detection and call material detection
 def detectEdge():
     print("Detecting Edge")
+
+    # Take image
     with picamera.PiCamera() as camera:
         output = np.empty((720, 1280, 3), dtype=np.uint8)
         camera.capture(output, 'rgb')
@@ -39,34 +44,33 @@ def detectEdge():
     mat = closestMaterial(output[0:100,590:690,:])
     print(mat)
     
-    imPic = np.mean(output, axis=2)
+    imPic = np.mean(output, axis=2) # Convert to grayscale
 
-    imBlur = gaussian_filter(imPic, sigma=3)
+    imBlur = gaussian_filter(imPic, sigma=3) # blur
     imThresh = np.copy(imBlur)
-    imThresh[np.logical_and(imThresh > 50, imThresh < 130)] = 0
+    imThresh[np.logical_and(imThresh > 50, imThresh < 130)] = 0 # Threshold
     
     
     imRaw = imBlur - imThresh
     
     vertSob = [[1,0,-1],[2,0,-2],[1,0,-1]]
-    #vertEdge = [[-1,2,-1],[-1,2,-1],[-1,2,-1]]
-    #totEdge = [[-1,-1,-1],[-1,8,-1],[-1,-1,-1]]
-    #blur = [[1/16, 1/8, 1/16],[1/8, 1/4, 1/8],[1/16, 1/16,1/8]]
-    #sharp = [[0,-1,0],[-1,5,-1],[0,-1,0]]
-    
+
     print("Thresholded Image")
     
-    im = fftconvolve(imRaw, vertSob, mode='same')
+    im = fftconvolve(imRaw, vertSob, mode='same') # FFT convolution with sobel filter
     
     print("Convolved Image")
     
+    # Threshold edges
     n = 80
     im[im < n] = 0
     im[im > n] = n
     
+    # Sum edges in each colomn
     eRow = im.sum(axis = 0) / 80
     eRow[0:5] = np.zeros((1,5))
     
+    # Find peaks of edge histogram
     peaks, _ = find_peaks(eRow, distance = 250)
     
     sorted_peaks = np.argsort(eRow[peaks])
@@ -74,6 +78,7 @@ def detectEdge():
         print("No edges found")
         return -1
     
+    # Find the 2 largest peaks
     pk1 = peaks[sorted_peaks[-1]]
     pk2 = peaks[sorted_peaks[-2]]
     
